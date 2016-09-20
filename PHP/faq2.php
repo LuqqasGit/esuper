b4886fdccc1b8f1767825e9a457f5eb4106dcac1
lucasdantur <lucasdantur@gmail.com>
2016-09-19 21:26:17 -0300

    PHP Folder

---
+++ PHP/faq2.php
@@ -0,0 +1,43 @@
+<?php
+$title = "eSUPER - FAQ";
+require_once("header.php");
+?>
+
+    <div class="main-container-bg-site">
+      <div class="main-container">
+        <div class="faq-main-container">
+            <h1 class="faq-title">Preguntas frecuentes</h1>
+            <section class="preguntas">
+                <article class="preguntas-y-respuestas">
+                    <h2>¿Qué es eSUPER?</h2>
+                    <p>Amet temporibus id consequuntur repudiandae facere tenetur vero aspernatur? Impedit magni assumenda laborum exercitationem facere. Magnam dignissimos nostrum fuga aperiam eos atque minima magnam adipisci, aliquam! Velit laudantium illo tenetur.</p>
+                </article>
+                <article class="preguntas-y-respuestas">
+                    <h2>¿Es necesario registrarme para efectuar una compra?</h2>
+                    <p>Lorem repudiandae libero molestiae fugit odio sit omnis perspiciatis id obcaecati qui reiciendis. Impedit laborum officia aperiam dolorem ad. Itaque error a explicabo voluptas cupiditate placeat maiores distinctio. Exercitationem libero!</p>
+                </article>
+                <article class="preguntas-y-respuestas">
+                    <h2>¿Es seguro este sitio?</h2>
+                    <p>Dolor sit dolorum porro reprehenderit animi repellat itaque. Ipsum dolorum magni quod porro veritatis debitis amet eius accusamus dignissimos illum. Veritatis consequuntur quibusdam esse omnis sunt magni alias est ullam!</p>
+                </article>
+                <article class="preguntas-y-respuestas">
+                    <h2>¿Cómo recibo el pedido?</h2>
+                    <p>Elit veritatis pariatur tempora omnis possimus cum tempora. Culpa eveniet maxime porro velit distinctio, iste blanditiis deleniti. Minima ab blanditiis incidunt ducimus qui. Error deserunt porro voluptas quia at? Distinctio?</p>
+                </article>
+                <article class="preguntas-y-respuestas">
+                    <h2>¿El servicio funciona únicamente para Capital Federal?</h2>
+                    <p>Ipsum quas reiciendis aspernatur atque eum porro quaerat perferendis impedit velit obcaecati. Quisquam expedita incidunt rem vero consequuntur vero ab temporibus necessitatibus, fugiat quidem? Praesentium delectus odio est facere a.</p>
+                </article>
+            </section>
+        </div><!-- .home-main-container -->
+      </div><!-- .main-container -->
+    </div><!-- .main-container-bg -->
+
+    <footer class="main-footer">
+      <a href=""><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+      <a href=""><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+      <a href=""><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+    </footer>
+    <script src="https://use.fontawesome.com/2390ddd5c7.js"></script>
+  </body>
+</html>
---
+++ PHP/login.php
@@ -0,0 +1,61 @@
+<?php
+$title = "eSUPER - Login";
+require_once("header.php");
+?>
+
+    <div class="main-container-bg-site">
+      <div class="main-container">
+        <div class="signup-main-container">
+
+          <div class="signup-text-box">
+            <h1 class="signup-title">Inicia sesión en eSuper</h1>
+          </div>
+
+          <div class="signup-box">
+            <div class="social-signup">
+              <div class="signup-fb"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></div>
+              <div class="signup-ggl"><i class="fa fa-google fa-2x" aria-hidden="true"></i></div>
+              <!-- <div class="signup-lnk"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></div> -->
+            </div>
+
+            <div class="signup-line"><span>ó</span></div>
+
+            <form class="signup-form" id="login" action="" method="post">
+              <fieldset>
+                <ul>
+                  <li>
+                    <input type="email" name="userlogin" placeholder="Correo electrónico / Usuario">
+                  </li>
+                  <div id="userlogin-validate-div" class="signup-validate-div-hidden"><p>Por favor ingresa un correo electrónico válido.</p></div>
+                  <li>
+                    <input type="password" name="password" placeholder="Contraseña">
+                  </li>
+                </ul>
+                <div class="login-menu">
+                  <div class="login-remember">
+                    <input type="checkbox" name="session" checked> <p>Recordarme</p>
+                  </div>
+                  <div class="login-forgot">
+                    <p><a class="signup-link" href="/forgot.html">¿Olvidaste tu contraseña?</a></p>
+                  </div>
+                </div>
+              </fieldset>
+              <button type="submit" class="signup-submit" name="submit">Continuar</button>
+            </form>
+
+            <div class="signup-footer">
+                <p>¿No tenés cuenta? <a class="signup-link" href="/signup.html">Registrate aquí</a> »</p>
+            </div>
+          </div> <!-- end signup-box -->
+        </div> <!-- end signup-main-container -->
+      </div>
+    </div>
+
+    <footer class="main-footer">
+      <a href=""><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+      <a href=""><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+      <a href=""><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+    </footer>
+    <script src="https://use.fontawesome.com/2390ddd5c7.js"></script>
+  </body>
+</html>
