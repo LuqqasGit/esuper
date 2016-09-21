822ee7ecc9730a00a19d1539f557f75e2fbdc0c3
LuqqasGit <lucas.villanueva12@gmail.com>
2016-09-21 10:40:31 -0300

    added footer elements

--- footer.php
+++ footer.php
@@ -1,7 +1,14 @@
 <footer class="main-footer">
-  <a href=""><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-  <a href=""><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
-  <a href=""><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+  <div class="main-footer-icons">
+    <a href=""><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+    <a href=""><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+    <a href=""><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+    <a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
+    <a href=""><i class="fa fa-share-square-o" aria-hidden="true"></i></a>
+  </div>
+  <div class="main-footer-copyright">
+    <p><?php echo date('Y'); ?> &copy;Copyright eSuper</p>
+  </div>
 </footer>
 </div>
 <script src="https://use.fontawesome.com/2390ddd5c7.js"></script>
