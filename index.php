431800a018edd6d9f8b729159bdf983be524dcd0
LuqqasGit <lucas.villanueva12@gmail.com>
2016-10-04 13:01:08 -0300

    Google Maps API - Location Form

--- index.php
+++ index.php
@@ -9,63 +9,18 @@ require_once("header.php");
           <h1 class="home-title">Al súper... Desde casa!</h1>
           <form class="location-form" action="" method="post">
             <div class="labels-div">
-            <label>Elegí tu barrio<br>
-              <select name="barrio">
-                <option value="01">Agronomía</option>
-                <option value="02">Almagro</option>
-                <option value="03">Balvanera</option>
-                <option value="04">Barracas</option>
-                <option value="05">Belgrano</option>
-                <option value="06">Boedo</option>
-                <option value="07">Caballito</option>
-                <option value="08">Chacarita</option>
-                <option value="09">Coghlan</option>
-                <option value="10">Colegiales</option>
-                <option value="11">Constitución</option>
-                <option value="12">Flores</option>
-                <option value="13">Floresta</option>
-                <option value="14">La Boca</option>
-                <option value="15">La Paternal</option>
-                <option value="16">Liniers</option>
-                <option value="17">Mataderos</option>
-                <option value="18">Monte Castro</option>
-                <option value="19">Montserrat</option>
-                <option value="20">Nueva Pompeya</option>
-                <option value="21">Nuñez</option>
-                <option value="22">Palermo</option>
-                <option value="23">Parque Avellaneda</option>
-                <option value="24">Parque Chacabuco</option>
-                <option value="25">Parque Chas</option>
-                <option value="26">Parque Patricios</option>
-                <option value="27">Puerto Madero</option>
-                <option value="28">Recoleta</option>
-                <option value="29">Retiro</option>
-                <option value="30">Saavedra</option>
-                <option value="31">San Cristóbal</option>
-                <option value="32">San Nicolás</option>
-                <option value="33">San Telmo</option>
-                <option value="34">Versalles</option>
-                <option value="35">Villa Crespo</option>
-                <option value="36">Villa Devoto</option>
-                <option value="37">Villa General Mitre</option>
-                <option value="38">Villa Lugano</option>
-                <option value="39">Villa Luro</option>
-                <option value="40">Villa Ortúzar</option>
-                <option value="41">Villa Pueyrredón</option>
-                <option value="42">Villa Real</option>
-                <option value="43">Villa Riachuelo</option>
-                <option value="44">Villa Santa Rita</option>
-                <option value="45">Villa Soldati</option>
-                <option value="46">Villa Urquiza</option>
-                <option value="47">Villa del Parque</option>
-                <option value="48">Vélez Sarsfield</option>
-              </select>
-            </label>
             <label>Escribí tu dirección<br>
-              <input type="text" name="direccion" placeholder="Ej. Av. Las Heras 1700">
+              <div id="locationField">
+                <input id="autocomplete" type="text" name="direccion" placeholder="Ej. Av. Las Heras 1700" onfocus="geolocate()">
+              </div>
             </label>
             </div>
-            <button type="submit" name="submit">Buscar</button>
+            <div class="button">
+              <button type="submit" name="submit">Buscar</button>
+            </div>
+            <div class="button">
+              <button><i class="fa fa-location-arrow" aria-hidden="true"></i> Mi Ubicación</button>
+            </div>
           </form>
           <section class="super-logo-container">
             <a href="">
