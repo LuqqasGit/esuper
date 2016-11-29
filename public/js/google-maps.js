67ad6d04dee34ab4bb9f5f56d1885f5d1fcd7bbf
LuqqasGit <lucas.villanueva12@gmail.com>
2016-11-29 15:48:53 -0300

    some more fixing

--- public/js/google-maps.js
+++ public/js/google-maps.js
@@ -41,11 +41,11 @@ function initMap() {
       map.setCenter(pos);
       map.setZoom(15);
     }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
+      // handleLocationError(true, infoWindow, map.getCenter());
     });
   } else {
     // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
+    // handleLocationError(false, infoWindow, map.getCenter());
   }
 
   setMarkers(map, markets, infoWindow);
@@ -136,8 +136,8 @@ function setMarkers (map, markets, infoWindow) {
   });
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
-  infoWindow.open(map);
-}
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//   infoWindow.setPosition(pos);
+//   infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+//   infoWindow.open(map);
+// }
